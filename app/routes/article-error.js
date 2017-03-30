a88ad2217dc928f9fb04658e96036853d0bbf59c
Igor Rogatty <igor@wikia-inc.com>
2017-03-30 15:27:32 +0200

    Version that works fine without FastBoot

--- app/routes/article-error.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-	setupController: function(controller, error) {
-		Ember.Logger.debug('article-error#setupController', error);
-		this._super(...arguments);
-	}
-});
--- app/routes/article.js
+++ app/routes/article.js
@@ -4,16 +4,10 @@ export default Ember.Route.extend({
 	fastboot: Ember.inject.service(),
 
 	model() {
-		this.get('fastboot').set('response.statusCode', 404);
+		if (this.get('fastboot.isFastBoot')) {
+			this.get('fastboot').set('response.statusCode', 404);
+		}
 
 		return Ember.RSVP.reject('404 error, this should not be visible to user');
-	},
-
-	actions: {
-		error() {
-			this.render('article-error');
-			// return true;
-			return false;
-		}
 	}
 });
